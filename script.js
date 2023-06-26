da328ab70a9701e176f951b4873e433861ce36ae
viniciusgjusta <viniciusxime@gmail.com>
2023-06-26 17:49:42 -0300

    Add weather Api and search-bar functions

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 let weather = {
-    "apiKey": "b705a642e2b89124aaf7fedc2caf4111"
-}
+    apiKey: "7e666ddaca8b26a620f939873fc900f2",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Tempo em " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Umidade: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Velocidade do vento: " + speed + "km/h";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
